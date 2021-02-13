bd7741d7a53efd63e0a7b21e6485d62594021a4d
Rhys <rhysrichmond1@gmail.com>
2021-02-13 11:33:37 +0000

    Made greybox scene with player prefab which can rotate the box horizontally/vertically

---
+++ EscapeRoom/Assets/Code/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject PuzzleBox;
+    private bool CanRotate = true;
+
+    private void Start()
+    {
+        PuzzleBox = GameObject.FindGameObjectWithTag("Box");
+    }
+
+    void Update()
+    {
+        MoveBox();
+    }
+
+    void MoveBox() 
+    {
+        if (Input.GetButtonDown("Horizontal")) 
+        {
+            if (CanRotate)
+                StartCoroutine(RotateBoxHorizontal());
+            
+        }
+
+        if (Input.GetButtonDown("Vertical")) 
+        {
+            if (CanRotate)
+                StartCoroutine(RotateBoxVertical());
+        }
+    }
+
+    IEnumerator RotateBoxHorizontal() 
+    {
+        CanRotate = false;
+        PuzzleBox.transform.Rotate(0, Input.GetAxisRaw("Horizontal") * 90, 0);
+        yield return new WaitForSeconds(1f);
+        CanRotate = true;
+    }
+
+    IEnumerator RotateBoxVertical()
+    {
+        CanRotate = false;
+        PuzzleBox.transform.Rotate(Input.GetAxisRaw("Vertical") * 90, 0, 0);
+        yield return new WaitForSeconds(1f);
+        CanRotate = true;
+    }
+}
