ffa9ba41491ba4252aa2d6c2ac8d06954482e805
Rhys <rhysrichmond1@gmail.com>
2021-03-08 11:27:25 +0000

    Completed rainbowtiles minigame and updated box prefab

---
+++ EscapeRoom/Assets/Code/Minigames/Simon/RainbowTiles.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowTiles : MonoBehaviour
+{
+    public int[] CurrentAttempt;
+    public string CurrentSolution;
+    public string CurrentAttemptString;
+
+    public GameObject CompleteObject;
+    public Material Green;
+    
+
+    private void Start()
+    {
+        for (int i = 0; i < 12; i++) 
+        {
+            int RandomID = Random.Range(0, 4);
+            Debug.Log(RandomID);
+            CurrentSolution += RandomID.ToString();
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void CheckAttempt() 
+    {
+        for (int i = 0; i < CurrentAttempt.Length; i++) 
+        {
+            CurrentAttemptString += CurrentAttempt[i].ToString();
+        }
+
+        if (CurrentAttemptString == CurrentSolution) 
+        {
+            CompleteObject.GetComponent<MeshRenderer>().material = Green;
+            //this.gameObject.GetComponent<RainbowTiles>().enabled = false;
+            this.enabled = false;
+        }
+    }
+}
---
+++ EscapeRoom/Assets/Code/Minigames/Simon/RainbowTilesButton.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowTilesButton : MonoBehaviour
+{
+    private RainbowTiles RT;
+    public int ButtonID, CurrentColour;
+    public Material[] Colours;
+
+    private void Start()
+    {
+        RT = this.transform.GetComponentInParent<RainbowTiles>();
+        if (gameObject.tag != "Check") 
+        {
+            CurrentColour = Random.Range(0, 4);
+            ChangeColour();
+        }
+    }
+
+    private void Update()
+    {
+        if (CurrentColour > 3)
+        {
+            CurrentColour = 0;
+            RT.CurrentAttempt[ButtonID] = CurrentColour;
+            ChangeColour();
+        }
+        
+
+    }
+
+    private void OnMouseOver() 
+    {
+       
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            if (gameObject.tag == "Check")
+            {
+                RT.CurrentAttemptString = "";
+                RT.CheckAttempt();
+                return;
+            }
+            CurrentColour++;
+            RT.CurrentAttempt[ButtonID] = CurrentColour;
+           
+            ChangeColour();
+        }
+
+        
+    }
+
+    void ChangeColour()
+    {
+        switch (CurrentColour)
+        {
+            case 0:
+                this.gameObject.GetComponent<MeshRenderer>().material = Colours[0];
+                break;
+            case 1:
+                this.gameObject.GetComponent<MeshRenderer>().material = Colours[1];
+                break;
+            case 2:
+                this.gameObject.GetComponent<MeshRenderer>().material = Colours[2];
+                break;
+            case 3:
+                this.gameObject.GetComponent<MeshRenderer>().material = Colours[3];
+                break;
+        }
+    }
+}
