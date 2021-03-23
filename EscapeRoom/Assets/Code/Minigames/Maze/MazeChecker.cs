6c9c4f43999528e5d5de96a9809aebabb3322f53
Rhys <rhysrichmond1@gmail.com>
2021-03-23 18:20:22 +0000

    Added invisible maze puzzle and prefabs of it and a box with it

---
+++ EscapeRoom/Assets/Code/Minigames/Maze/MazeChecker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeChecker : MonoBehaviour
+{
+    public GameObject CompleteLight;
+    public Material Green;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MazePlayer") 
+        {
+            CompleteLight.GetComponent<Renderer>().material = Green;
+        }
+    }
+}
---
+++ EscapeRoom/Assets/Code/Minigames/Maze/MazeMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeMovement : MonoBehaviour
+{
+    public float moveSpeed, Distance, CircleCollider;
+    public string Button;
+    public Transform movePoint;
+    public LayerMask Collision;
+
+
+    public void Update()
+    {
+        if (Button != "")
+            return;
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
+
+    public void OnMouseOver()
+    {
+        if (Button == "")
+            return;
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            if (Button == "Left") 
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Distance, 0, 0),CircleCollider,Collision))
+                {
+                    movePoint.position += new Vector3(Distance, 0, 0);
+                }
+            }
+
+            if (Button == "Right")
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(-Distance, 0, 0), CircleCollider, Collision))
+                    movePoint.position += new Vector3(-Distance, 0, 0);
+            }
+
+            if (Button == "Up") 
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0, Distance, 0), CircleCollider, Collision))
+                    movePoint.position += new Vector3(0, Distance, 0);
+            }
+
+            if (Button == "Down")
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0, -Distance, 0), CircleCollider, Collision))
+                    movePoint.position += new Vector3(0, -Distance, 0);
+            }
+                
+        }
+    }
+}
